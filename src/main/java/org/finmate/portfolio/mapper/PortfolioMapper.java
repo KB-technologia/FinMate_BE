2f24844fc5f36dea23a31e086134032ec1368493
Youngjae so <terranbang@gmail.com>
2025-07-18 03:01:20 +0900

    ✨  재무 포트폴리오 Mapper 인터페이스 추가

---
+++ src/main/java/org/finmate/portfolio/mapper/PortfolioMapper.java
@@ -0,0 +1,17 @@
+package org.finmate.portfolio.mapper;
+
+import org.finmate.portfolio.domain.PortfolioVO;
+
+import java.util.List;
+
+public interface PortfolioMapper {
+
+    public PortfolioVO selectPortfolioByUserId(Long userId);
+
+    public int insertPortfolio(PortfolioVO vo);
+
+    public int deletePortfolioByUserId(Long userId);
+
+    public int updatePortfolio(PortfolioVO portfolio);
+
+}
