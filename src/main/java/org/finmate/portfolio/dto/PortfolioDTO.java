3286fc7b93524f9f59630b4b7894750533413ea8
Youngjae so <terranbang@gmail.com>
2025-07-18 02:51:17 +0900

    ♻️ Swagger문서 추가 및 DTO 수정

--- src/main/java/org/finmate/portfolio/dto/PortfolioDTO.java
+++ src/main/java/org/finmate/portfolio/dto/PortfolioDTO.java
@@ -1,5 +1,8 @@
 package org.finmate.portfolio.dto;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,16 +13,37 @@ import org.finmate.portfolio.domain.PortfolioVO;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "재무 포트폴리오 DTO")
 public class PortfolioDTO {
+
+    @ApiModelProperty(value = "재무 포트폴리오 ID")
     private Long id;
+
+    @ApiModelProperty(value = "사용자 ID(FK)")
     private Long userId;
+
+    @ApiModelProperty(value = "사용자 나이")
     private Integer age;
+
+    @ApiModelProperty(value = "사용자 자산 총액", example="50000000.0")
     private Double totalAssets;
+
+    @ApiModelProperty(value = "사용자 연 소득", example="10000000.0")
     private Double annualIncome;
+
+    @ApiModelProperty(value = "사용자 투자 성향", example="안정형")
     private String investmentProfile;
+
+    @ApiModelProperty(value = "현금,예금 비율", example="10.0")
     private Double cashRatio;
+
+    @ApiModelProperty(value = "채권 비율", example="20.0")
     private Double bondRatio;
+
+    @ApiModelProperty(value = "주식,펀드 비율", example="40.0")
     private Double equityRatio;
+
+    @ApiModelProperty(value = "기타 비율", example="30.0")
     private Double otherRatio;
 
     public static PortfolioDTO of(PortfolioVO vo) {
@@ -38,7 +62,6 @@ public class PortfolioDTO {
     }
     public PortfolioVO toVO() {
         return PortfolioVO.builder()
-                .id(id)
                 .userId(userId)
                 .age(age)
                 .totalAssets(totalAssets)
@@ -49,6 +72,5 @@ public class PortfolioDTO {
                 .equityRatio(equityRatio)
                 .otherRatio(otherRatio)
                 .build();
-
     }
 }
