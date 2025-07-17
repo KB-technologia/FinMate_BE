a86d001773c904d4508a4addf276e1bd20c4dd07
ckdwlsrh <ckdwls6504@gmail.com>
2025-07-17 14:05:47 +0900

    :sparkles: 상세보기 서비스 및 컨트롤러 구성 완료 - 매퍼에서 조인 잘못된 코드 수정 - 상세보기 API 명세서 작성 - 상품 ID를 통해서 공통 부분 및 detail속성으로 json파일 반환 - issue: #1, #3

--- src/main/java/org/finmate/product/controller/ProductController.java
+++ src/main/java/org/finmate/product/controller/ProductController.java
@@ -1,6 +1,12 @@
 package org.finmate.product.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.finmate.product.dto.ProductDTO;
 import org.finmate.product.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,13 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/product")
+@Log4j2
+@Api(tags = "금융 상품 API")
 public class ProductController {
     private final ProductService productService;
 
     //TODO: 전체 상품 API 구현
 
-//    @GetMapping("/{id}")
-//    public ResponseEntity<T> getProductDetail(@PathVariable Long id) {
-//        return ResponseEntity.ok();
-//    }
+    @ApiOperation(value = "금융 상품 상세 보기", notes = "금융 상품 ID로 상세 정보를 불러오는 API")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "성공적으로 요청이 처리되었습니다.", response = ProductDTO.class),
+            @ApiResponse(code = 400, message = "잘못된 요청입니다."),
+            @ApiResponse(code = 500, message = "서버에서 오류가 발생했습니다.")
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO> getProductDetail(@PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductDetail(id));
+    }
 }
