4866dc71017755c9f3097e6afa5bd93259892a33
yeainlee <lyy923923@gmail.com>
2025-07-17 15:42:31 +0900

    :sparkles: security/config/SecurityConfig-ã문자셋필터 추가

--- src/main/java/org/finmate/auth/security/config/SecurityConfig.java
+++ src/main/java/org/finmate/auth/security/config/SecurityConfig.java
@@ -1,11 +1,26 @@
 package org.finmate.auth.security.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    // 문자셋 필터
+    public CharacterEncodingFilter encodingFilter(){
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+        encodingFilter.setForceEncoding(true);
+        return encodingFilter;
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http.addFilterBefore(encodingFilter(), CsrfFilter.class);
+    }
 
 }
