b58cb3d86d17e4550882d648ab2ed6808f9730ff
ckdwlsrh <ckdwls6504@gmail.com>
2025-07-17 10:53:29 +0900

    :sparkles: 상품 컨트롤러 및 서비스 생성 - 금융 상품 관련 빈 컨트롤러, 서비스 생성 및 설정 - 메서드 작성 바람 - issue: #1

--- src/main/java/org/finmate/config/RootConfig.java
+++ src/main/java/org/finmate/config/RootConfig.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -17,6 +18,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @PropertySource({"classpath:/application.properties"})
+@ComponentScan(basePackages = {"org.finmate.product.service"})
 @MapperScan(basePackages = {
         "org.finmate.product.mapper"
 })
