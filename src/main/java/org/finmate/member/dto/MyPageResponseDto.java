2d6abeac436909c772839a086d8c9bdbf4be72e7
yeni023 <yeeun3011@naver.com>
2025-07-18 13:15:54 +0900

    :memo: MyPage 관련 Controller 및 DTO Swagger 어노테이션 추가

--- src/main/java/org/finmate/member/dto/MyPageResponseDto.java
+++ src/main/java/org/finmate/member/dto/MyPageResponseDto.java
@@ -1,5 +1,7 @@
 package org.finmate.member.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,9 +10,16 @@ import org.finmate.member.domain.MyPage;
 @Getter
 @Builder
 @AllArgsConstructor
+@ApiModel(description = "마이페이지 조회 응답 DTO")
 public class MyPageResponseDto {
+
+    @ApiModelProperty(value = "계정 ID", example = "user123")
     private String accountId;
+
+    @ApiModelProperty(value = "이메일", example = "user@example.com")
     private String email;
+
+    @ApiModelProperty(value = "생년월일", example = "1990-01-01")
     private String birth;
 
     public static MyPageResponseDto from(MyPage myPage) {
--- src/main/java/org/finmate/member/dto/MyPageUpdateRequestDto.java
+++ src/main/java/org/finmate/member/dto/MyPageUpdateRequestDto.java
@@ -1,14 +1,23 @@
 package org.finmate.member.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 import org.finmate.member.domain.MyPage;
 
 @Getter
 @Setter
+@ApiModel(description = "마이페이지 수정 요청 DTO")
 public class MyPageUpdateRequestDto {
+
+    @ApiModelProperty(value = "새 비밀번호", example = "newPassword123")
     private String password;
+
+    @ApiModelProperty(value = "수정할 이메일", example = "newuser@example.com")
     private String email;
+
+    @ApiModelProperty(value = "수정할 생년월일", example = "1995-05-01")
     private String birth;
 
     public MyPage toDomainWithAccountId(String accountId) {
