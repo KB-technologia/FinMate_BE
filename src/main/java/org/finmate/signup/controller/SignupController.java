3d4f9911125982c0bbd572605f105869ff341c62
Yuchan Oh <dhcks0861@naver.com>
2025-07-18 13:10:40 +0900

    :recycle: 회원가입 API (DTO Controller Swagger 작성)

--- src/main/java/org/finmate/signup/controller/SignupController.java
+++ src/main/java/org/finmate/signup/controller/SignupController.java
@@ -1,11 +1,13 @@
 package org.finmate.signup.controller;
 
+import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.finmate.signup.dto.SignupRequestDto;
 import org.finmate.signup.service.SignupService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Api(tags = "회원가입 API")
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
@@ -13,6 +15,11 @@ public class SignupController {
 
     private final SignupService signupService;
 
+    @ApiOperation(value = "회원가입", notes = "회원 정보를 입력받아 회원가입을 수행합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "회원가입 성공"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
     @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody SignupRequestDto dto) {
         signupService.signup(dto);
--- src/main/java/org/finmate/signup/dto/SignupRequestDto.java
+++ src/main/java/org/finmate/signup/dto/SignupRequestDto.java
@@ -1,20 +1,31 @@
 package org.finmate.signup.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ApiModel(value = "회원가입 요청", description = "회원가입 시 필요한 사용자 정보 DTO")
 public class SignupRequestDto {
+
+    @ApiModelProperty(value = "사용자 이름", example = "홍길동")
     private String name;
+
+    @ApiModelProperty(value = "사용자 계정 ID", example = "hong123")
     private String accountId;
+
+    @ApiModelProperty(value = "이메일 주소", example = "hong@example.com")
     private String email;
+
+    @ApiModelProperty(value = "비밀번호", example = "1234abcd")
     private String password;
-    private LocalDate birth;
+
+    @ApiModelProperty(value = "생년월일", example = "2000-01-01")
+    private String birth;
 }
