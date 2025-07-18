09bc9256605068072ea4717a9792817ecd192070
ckdwlsrh <ckdwls6504@gmail.com>
2025-07-18 11:45:06 +0900

    :sparkles: 비교 서비스 및 컨트롤러 개발 - 서비스 및 컨트롤러 작성 완료 - 사용자 데이터 작성 완료 되면 연동해야됨 - issue: #9

--- src/main/java/org/finmate/product/controller/ProductController.java
+++ src/main/java/org/finmate/product/controller/ProductController.java
@@ -19,20 +19,6 @@ public class ProductController {
 
     //TODO: 전체 상품 API 구현
 
-    @ApiOperation(value = "금융 상품 상세 보기", notes = "금융 상품 ID로 상세 정보를 불러오는 API")
-    @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "성공적으로 요청이 처리되었습니다.", response = ProductDTO.class),
-            @ApiResponse(code = 400, message = "잘못된 요청입니다."),
-            @ApiResponse(code = 500, message = "서버에서 오류가 발생했습니다.")
-    })
-    @GetMapping("/{id}")
-    public ResponseEntity<ProductDTO> getProductDetail(
-            @ApiParam(value = "상품 ID", required = true, example = "1")
-            @PathVariable Long id) {
-        return ResponseEntity.ok(productService.getProductDetail(id));
-    }
-
-
 
     @ApiOperation(value = "금융 상품 비교", notes = "금융 상품을 비교하는 API")
     @ApiResponses(value = {
@@ -46,8 +32,22 @@ public class ProductController {
             @RequestParam Long id1,
             @ApiParam(value = "상품 ID 2", required = true, example = "2")
             @RequestParam Long id2) {
-        //TODO: 로직 작성
-
-        return ResponseEntity.ok().body(new ProductComparisonResultDTO());
+        return ResponseEntity.ok(productService.compareProducts(id1, id2));
     }
+
+
+    @ApiOperation(value = "금융 상품 상세 보기", notes = "금융 상품 ID로 상세 정보를 불러오는 API")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "성공적으로 요청이 처리되었습니다.", response = ProductDTO.class),
+            @ApiResponse(code = 400, message = "잘못된 요청입니다."),
+            @ApiResponse(code = 500, message = "서버에서 오류가 발생했습니다.")
+    })
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductDTO<?>> getProductDetail(
+            @ApiParam(value = "상품 ID", required = true, example = "1")
+            @PathVariable Long id) {
+        return ResponseEntity.ok(productService.getProductDetail(id));
+    }
+
+
 }
