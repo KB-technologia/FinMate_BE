0316f663fe3167ca4c5ad828f49b6488bcdf18db
Youngjae so <terranbang@gmail.com>
2025-07-18 11:44:35 +0900

    ♻️ 재무 포트폴리오 DTO 리팩토링

--- src/main/java/org/finmate/portfolio/dto/PortfolioDTO.java
+++ src/main/java/org/finmate/portfolio/dto/PortfolioDTO.java
@@ -1,6 +1,5 @@
 package org.finmate.portfolio.dto;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
