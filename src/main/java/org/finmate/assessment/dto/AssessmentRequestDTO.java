911b2b570ac3bc7ee78682071387b23fab6daefe
mojiri00 <moguricamera@gmail.com>
2025-07-20 00:10:09 +0900

    ♻️refactor: AssessmentRequestDTO 스웨거 추가 - AssessmentRequestDTO 스웨거 추가

--- src/main/java/org/finmate/assessment/dto/AssessmentRequestDTO.java
+++ src/main/java/org/finmate/assessment/dto/AssessmentRequestDTO.java
@@ -1,18 +1,24 @@
 package org.finmate.assessment.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
 
-@Data
 
 // 사용자로부터 테스트 선택지를 받기 위한 DTO
+@Data
+@ApiModel(description = "사용자로부터 테스트 선택지를 받기 위한 DTO")
 public class AssessmentRequestDTO {
 
     /**
      * userId는 추후 토큰 형식으로 리팩토링 필요
     **/
 
+    @ApiModelProperty(value = "사용자 ID", example = "1")
     private Long userId; // 사용자 ID
+
+    @ApiModelProperty(value = "사용자가 선택한 선택지에 따른 점수 리스트")
     private List<Integer> choices; // 사용자가 선택한 선택지들
 }
