90dfba8dbc57f088d640720db6096453e5896110
yeainlee <lyy923923@gmail.com>
2025-07-20 22:53:29 +0900

    :sparkles: API 서버용 security 기본 설정 변경 - CorsFilter 등록 - CSRF, formLogin, httpBasic 비활성화

--- src/main/java/org/finmate/security/config/SecurityConfig.java
+++ src/main/java/org/finmate/security/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.finmate.security.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.finmate.auth.handler.LoginFailureHandler;
 import org.finmate.auth.handler.LoginSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,10 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,29 +23,28 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.filter.CorsFilter;
 
 import javax.servlet.http.HttpServletResponse;
 
+@Log4j2
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
 @ComponentScan(basePackages = {"org.finmate.auth.service", "org.finmate.auth", "org.finmate.security"})
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
-    private final LoginSuccessHandler loginSuccessHandler;
-    private final LoginFailureHandler loginFailureHandler;
-    private final LogoutSuccessHandler logoutSuccessHandler;
     private final UserDetailsService userDetailsService;
-
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
-    // 문자셋 필터
+    // 문자셋 필터 (한글 인코딩 필터)
     public CharacterEncodingFilter encodingFilter(){
         CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
         encodingFilter.setEncoding("UTF-8");
@@ -50,30 +52,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return encodingFilter;
     }
 
+    // AuthenticationManager 빈 등록
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    // cross origin 접근 허용
+    @Bean
+    public CorsFilter corsFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOriginPattern("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+
+    // 접근 제한 무시 경로 설정 - resource
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers("/assets/**", "/api/member/**");
+    }
+
+    // Http 보안 설정 (JWT 기반)
     @Override
     public void configure(HttpSecurity http) throws Exception{
-        //http.addFilterBefore(encodingFilter(), CsrfFilter.class);
-        http.csrf().disable();
+        //한글 인코딩 필터 설정
+        http.addFilterBefore(encodingFilter(), CsrfFilter.class);
 
         // 경로별 접근 권한 설정
-        http.authorizeRequests()
-                .antMatchers("/security/all/**").permitAll()
-                .antMatchers("/security/admin/**").access("hasRole('ADMIN')")
-                .antMatchers("/security/member/**").access("hasAnyRole('MEMBER', 'ADMIN')");
-
-        // 임시 테스트용
-        http.formLogin()
-            .loginProcessingUrl("/security/login")
-            .successHandler(loginSuccessHandler)
-            .failureHandler(loginFailureHandler);
-
-        http.logout()
-                .logoutUrl("/security/logout")
-                .logoutSuccessHandler(logoutSuccessHandler)
-                .invalidateHttpSession(true)
-                .deleteCookies("remember-me", "JSESSIONID");
-
-
+        http.httpBasic().disable() // 기본 HTTP 인증비활성화
+                .csrf().disable()  // CSRF 비활성화
+                .formLogin().disable()  // formLogin 비활성화- 관련 필터 해제
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 세션 생성 모드 설정
     }
 
 
@@ -82,11 +97,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(passwordEncoder());
 
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception{
-        return super.authenticationManagerBean();
     }
 }
