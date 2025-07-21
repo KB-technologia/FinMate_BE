35d821f13be89ce05b13cbf81236de44491e5d42
Youngjae so <terranbang@gmail.com>
2025-07-21 13:52:19 +0900

    ✨ 퀴즈 Service 추가

---
+++ src/main/java/org/finmate/quiz/service/QuizServiceImpl.java
@@ -0,0 +1,32 @@
+package org.finmate.quiz.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.finmate.quiz.domain.QuizVO;
+import org.finmate.quiz.dto.QuizCheckRequestDTO;
+import org.finmate.quiz.dto.QuizDTO;
+import org.finmate.quiz.mapper.QuizMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class QuizServiceImpl implements QuizService {
+    private final QuizMapper quizMapper;
+
+    public QuizDTO getRandomQuiz(){
+        QuizVO randomQuiz = quizMapper.selectRandomQuiz();
+        return QuizDTO.of(randomQuiz);
+    }
+
+    public String checkAnswer(QuizCheckRequestDTO dto) {
+        QuizVO quiz = quizMapper.selectQuiz(dto.getQuizId());
+        log.info(quiz.isQuizAnswer());
+        log.info(dto.isAnswer());
+        if(quiz.isQuizAnswer() == dto.isAnswer()){
+            return quiz.getCorrectAnswer();
+        } else {
+            return quiz.getWrongAnswer();
+        }
+    }
+}
