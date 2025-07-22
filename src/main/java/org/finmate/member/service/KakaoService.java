49e860e63d25db1353f27e57ef2f9f5c059b50cc
yeainlee <lyy923923@gmail.com>
2025-07-22 12:00:52 +0900

    :sparkles: Provider 위치 변경, POST 로그인 요청 url 변경 - 이전 : api/auth/login - 이후 : auth/login

--- src/main/java/org/finmate/member/service/KakaoService.java
+++ src/main/java/org/finmate/member/service/KakaoService.java
@@ -1,4 +1,12 @@
 package org.finmate.member.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class KakaoService {
+
+    private final String CLIENT_ID = "카카오 REST API 키";
+    private final String REDIRECT_URI = "http://localhost:8080/auth/kakao/callback";
 }
