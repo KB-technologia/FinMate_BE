cbcbab3d52b42535ea0127ee70480f89ba5b9776
mojiri00 <moguricamera@gmail.com>
2025-07-22 14:50:03 +0900

    ♻️refactor: 사용자 ID 요청 방식 변경 - AssessmentController.java CustomUser 을 활용하여 프론트에서 직접 사용자 아이디를 받지 않고 토큰 방식을 통해 직접 사용자 ID를 가져오도록 수정.

--- src/main/java/org/finmate/assessment/dto/AssessmentRequestDTO.java
+++ src/main/java/org/finmate/assessment/dto/AssessmentRequestDTO.java
@@ -12,13 +12,6 @@ import java.util.List;
 @ApiModel(description = "사용자로부터 테스트 선택지를 받기 위한 DTO")
 public class AssessmentRequestDTO {
 
-    /**
-     * userId는 추후 토큰 형식으로 리팩토링 예정
-    **/
-
-    @ApiModelProperty(value = "사용자 ID", example = "1")
-    private Long userId; // 사용자 ID
-
     @ApiModelProperty(value = "사용자가 선택한 선택지에 따른 점수 리스트")
     private List<Integer> choices; // 사용자가 선택한 선택지들
 }
