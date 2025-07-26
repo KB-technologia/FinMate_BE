0ff366eadbf7a5e35f2951432767687490bcd4c8
Yuchan Oh <dhcks0861@naver.com>
2025-07-26 10:05:08 +0900

    :recycle: 불필요한 import 제거

--- src/main/java/org/finmate/config/RootConfig.java
+++ src/main/java/org/finmate/config/RootConfig.java
@@ -13,12 +13,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
-import java.util.Locale;
 
 @Configuration
 @EnableTransactionManagement
