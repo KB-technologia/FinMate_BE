c4141c57fdbb4831cb31ec195bd83ecd342093b0
mojiri00 <moguricamera@gmail.com>
2025-07-27 22:05:32 +0900

    ♻️refactor: ProductRecommendationController 스웨거 추가

--- src/main/java/org/finmate/product/controller/ProductRecommendationController.java
+++ src/main/java/org/finmate/product/controller/ProductRecommendationController.java
@@ -1,6 +1,9 @@
 package org.finmate.product.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.finmate.product.dto.ProductDTO;
@@ -23,6 +26,11 @@ public class ProductRecommendationController {
 
     private final ProductMapper productMapper;
 
+    @ApiOperation(value = "랜덤 상품 추천", notes = "메인 화면에서 무작위로 금융 상품 8개를 추천합니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "정상적으로 랜덤 추천 상품을 반환했습니다."),
+            @ApiResponse(code = 500, message = "서버 오류 발생")
+    })
     @GetMapping("/random")
     ResponseEntity<List<ProductDTO<?>>> getRandomProductRecommendation(){
         return ResponseEntity.ok(productMapper.getRandomProductRecommendation()
