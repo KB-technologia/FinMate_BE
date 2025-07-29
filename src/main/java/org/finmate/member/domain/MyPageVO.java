580701bb6331698dbcd84b900a537a42486ec648
yeni023 <yeeun3011@naver.com>
2025-07-29 23:05:23 +0900

    ✨캐릭터 정보 필드 추가

--- src/main/java/org/finmate/member/domain/MyPageVO.java
+++ src/main/java/org/finmate/member/domain/MyPageVO.java
@@ -12,18 +12,21 @@ public class MyPageVO {
     private final String accountId;
     private final String email;
     private final String birth;
+    private final String animalName;
+    private final String animalImage;
 
-    public MyPageVO(String accountId, String email, String birth) {
+    public MyPageVO(String accountId, String email, String birth, String animalName, String animalImage) {
         this.accountId = accountId;
         this.email = email;
         this.birth = birth;
+        this.animalName = animalName;
+        this.animalImage = animalImage;
     }
-
     public MyPageVO updateEmail(String newEmail) {
-        return new MyPageVO(this.accountId, newEmail, this.birth);
+        return new MyPageVO(this.accountId, newEmail, this.birth, this.animalName, this.animalImage);
     }
 
     public MyPageVO updateBirth(String newBirth) {
-        return new MyPageVO(this.accountId, this.email, newBirth);
+        return new MyPageVO(this.accountId, this.email, newBirth, this.animalName, this.animalImage);
     }
 }
