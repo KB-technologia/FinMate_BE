54665ba4bf71d6e8c0b1fe53bf5ce021462bd74c
Youngjae so <terranbang@gmail.com>
2025-08-04 10:31:23 +0900

    ♻️ 재무 포트폴리오 검색 조건 변경

--- src/main/java/org/finmate/portfolio/service/PortfolioService.java
+++ src/main/java/org/finmate/portfolio/service/PortfolioService.java
@@ -1,7 +1,5 @@
 package org.finmate.portfolio.service;
 
-import org.finmate.adapter.mydata.dto.MyDataResponseDTO;
-import org.finmate.portfolio.domain.InvestmentProfile;
 import org.finmate.portfolio.dto.PortfolioDTO;
 import org.finmate.portfolio.dto.PortfolioRequestDTO;
 
@@ -10,7 +8,7 @@ import java.time.LocalDate;
 public interface PortfolioService {
     void createPortfolio(Long userId, PortfolioRequestDTO portfolioRequestDTO);
     PortfolioDTO getPortfolioByUserId(Long userId);
-    void updatePortfolio(PortfolioDTO portfolio);
+    void updatePortfolio(Long userId, PortfolioRequestDTO portfolioRequestDTO);
     void deletePortfolioByUserId(Long userId);
 
     PortfolioDTO getHistoryPortfolioByUserId(Long userId, LocalDate date);
