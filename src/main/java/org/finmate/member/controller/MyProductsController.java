386ccb9d19edbf5b7f953aee1bff80a14bc211c9
mojiri00 <moguricamera@gmail.com>
2025-08-04 09:33:15 +0900

    ♻️ 사용자 가입 상품 조회 메소드 변경 - MyDataApi 주입하여 getMyData 메소드 사용

--- src/main/java/org/finmate/member/controller/MyProductsController.java
+++ src/main/java/org/finmate/member/controller/MyProductsController.java
@@ -3,12 +3,17 @@ package org.finmate.member.controller;
 
 import io.swagger.annotations.Api;
 import lombok.RequiredArgsConstructor;
+import org.finmate.adapter.mydata.MyDataApi;
+import org.finmate.adapter.mydata.dto.MyDataResponseDTO;
+import org.finmate.member.domain.CustomUser;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,9 +21,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "가입 상품 API")
 public class MyProductsController {
 
+    private final MyDataApi myDataApi;
+
     @GetMapping("/myproducts")
-    public ResponseEntity<Resource> getMyProductsList(){
-        Resource res = new ClassPathResource("dummy/mydata/user_financial_products.json");
-        return ResponseEntity.ok(res); // JSON 그대로 전송
+    public ResponseEntity<MyDataResponseDTO> getMyProductsList(
+            @ApiIgnore @AuthenticationPrincipal CustomUser customUser
+    ){
+        Long userId = customUser.getUser().getId();
+        return ResponseEntity.ok(myDataApi.getMyData(userId)); // JSON 그대로 전송
     }
 }
