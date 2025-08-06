389ab0993066ae3712e805edc7c52f53feab9e43
ckdwlsrh <ckdwls6504@gmail.com>
2025-08-06 13:25:12 +0900

    :bug: 오버라이딩 메소드 정의 수정 - resetDailyAttendanceRewards라는 메소드가 인터페이스에 정의되어있지않아 빈으로 생성이되지않음 - 인터페이스에 resetDailyAttendanceRewards 메소드 생성 및 오버라이딩 처리

--- src/main/java/org/finmate/attendance/service/UserAttendanceService.java
+++ src/main/java/org/finmate/attendance/service/UserAttendanceService.java
@@ -3,6 +3,7 @@ package org.finmate.attendance.service;
 import org.finmate.attendance.dto.UserAttendanceDTO;
 
 public interface UserAttendanceService {
+    void resetDailyAttendanceRewards();
     UserAttendanceDTO getUserAttendance(Long userId);
     void checkInAttendance(Long userId);
 }
