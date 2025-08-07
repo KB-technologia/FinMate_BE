6154631cfc1afecd23218491ed4f6f6794906ac2
Youngjae so <terranbang@gmail.com>
2025-08-07 19:42:59 +0900

    ♻️ MyData를 통한 재무 포트폴리오 데이터 생성 관련 로직 수정

--- src/main/java/org/finmate/adapter/mydata/MyDataApi.java
+++ src/main/java/org/finmate/adapter/mydata/MyDataApi.java
@@ -1,17 +1,13 @@
 package org.finmate.adapter.mydata;
 
-import aj.org.objectweb.asm.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
-import org.finmate.adapter.mydata.dto.MyDataProductDTO;
 import org.finmate.adapter.mydata.dto.MyDataResponseDTO;
 import org.finmate.exception.NotFoundException;
-import org.finmate.member.domain.UserVO;
-import org.finmate.member.dto.KakaoUserResponse;
 import org.finmate.member.mapper.UserMapper;
+import org.finmate.portfolio.domain.PortfolioVO;
 import org.finmate.portfolio.dto.PortfolioDTO;
 import org.finmate.portfolio.mapper.PortfolioMapper;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -19,32 +15,44 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class MyDataApi {
 
     private final ObjectMapper objectMapper;
-
     private final UserMapper userMapper;
     private final PortfolioMapper portfolioMapper;
 
-    @Scheduled(cron = "0 0 1 * * *")
+    @Scheduled(cron = "0 0 9 * * *", zone = "Asia/Seoul")
+    //Todo : cron = "0 0 1 * * *", zone = "Asia/Seoul"으로 바꾸기
+
     @Transactional
     public void fetchMyData() {
         List<Long> userList = userMapper.getUserIdAll();
         for(Long userId : userList) {
-            PortfolioDTO dto = PortfolioDTO.from(portfolioMapper.getPortfolio(userId));
-
             PortfolioDTO newDto = getMyData(userId).toPortfolioDTO();
             newDto.setUserId(userId);
-            newDto.setCash(dto.getCash());
-            newDto.setOther(dto.getOther());
-            newDto.update();
 
+            PortfolioVO lastData = portfolioMapper.getPortfolio(userId);
+            PortfolioDTO dto = (lastData != null) ? PortfolioDTO.from(lastData) : null;
+
+            if (dto != null) {
+                if (dto.getDeposit() != null) newDto.setDeposit(dto.getDeposit());
+                if (dto.getSavings() != null) newDto.setSavings(dto.getSavings());
+                if (dto.getBond() != null) newDto.setBond(dto.getBond());
+                if (dto.getFund() != null) newDto.setFund(dto.getFund());
+                if (dto.getStock() != null) newDto.setStock(dto.getStock());
+                if (dto.getCash() != null) newDto.setCash(dto.getCash());
+                if (dto.getOther() != null) newDto.setOther(dto.getOther());
+            }
+
+            if (newDto.getCash() == null) newDto.setCash(0.0);
+            if (newDto.getOther() == null) newDto.setOther(0.0);
+
+            newDto.setTotalAssets(newDto.getCash() + newDto.getOther() + newDto.getStock() + newDto.getFund() +
+                    newDto.getBond() + newDto.getSavings() + newDto.getDeposit());
             portfolioMapper.insertPortfolio(newDto.toVO());
         }
     }
