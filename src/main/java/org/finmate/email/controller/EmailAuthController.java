c3ed21e01d692b8fdbc2506b7a08f2308059b363
ckdwlsrh <ckdwls6504@gmail.com>
2025-08-07 10:53:51 +0900

    :recycle: 이메일 관련 API 리팩토링 - final 매개변수 추가 - 스웨거 문서 수정 - issue: #100

--- src/main/java/org/finmate/email/controller/EmailAuthController.java
+++ src/main/java/org/finmate/email/controller/EmailAuthController.java
@@ -20,12 +20,7 @@ public class EmailAuthController {
 
     @ApiOperation(value = "이메일 인증 요청", notes = "사용자의 이메일로 인증 코드를 전송합니다.")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "인증코드 발송 성공", response = EmailAuthInitResponseDTO.class),
-            @ApiResponse(code = 400, message = "잘못된 요청"),
-            @ApiResponse(code = 401, message = "인증 실패"),
-            @ApiResponse(code = 403, message = "권한 없음"),
-            @ApiResponse(code = 404, message = "리소스를 찾을 수 없음"),
-            @ApiResponse(code = 500, message = "서버 오류")
+            @ApiResponse(code = 200, message = "인증코드 발송 성공", response = EmailAuthInitResponseDTO.class)
     })
     @PostMapping( "/authentication")
     public ResponseEntity<EmailAuthInitResponseDTO> sendAuthCode(
@@ -38,12 +33,7 @@ public class EmailAuthController {
 
     @ApiOperation(value = "이메일 인증 코드 검증", notes = "UUID와 인증 코드를 통해 이메일 인증을 검증합니다.")
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "인증 성공 여부 반환", response = Boolean.class),
-            @ApiResponse(code = 400, message = "잘못된 요청"),
-            @ApiResponse(code = 401, message = "인증 실패"),
-            @ApiResponse(code = 403, message = "권한 없음"),
-            @ApiResponse(code = 404, message = "리소스를 찾을 수 없음"),
-            @ApiResponse(code = 500, message = "서버 오류")
+            @ApiResponse(code = 200, message = "인증 성공 여부 반환", response = Boolean.class)
     })
     @PostMapping( "/authentication/verify")
     public ResponseEntity<Boolean> verifyAuthCode(
