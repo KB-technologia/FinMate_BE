706389d199badb684afcf0850c00143c8bb3fd27
Youngjae so <terranbang@gmail.com>
2025-08-08 12:40:37 +0900

    ♻️ 코드 일부 롤백 및 투자 성향 로직 제거

--- src/main/java/org/finmate/portfolio/dto/PortfolioDTO.java
+++ src/main/java/org/finmate/portfolio/dto/PortfolioDTO.java
@@ -81,5 +81,19 @@ public class PortfolioDTO {
                 .build();
     }
 
+    /**
+     * InvestmentProfile(투자성향) 계산 & 저장된 자산의 총합을 구하는 메서드 (로직 수정 필요)
+     */
+    public void update() {
+        double deposit = this.deposit == null ? 0.0 : this.deposit;
+        double savings = this.savings == null ? 0.0 : this.savings;
+        double bond = this.bond == null ? 0.0 : this.bond;
+        double fund = this.fund == null ? 0.0 : this.fund;
+        double stock = this.stock == null ? 0.0 : this.stock;
+        double cash = this.cash == null ? 0.0 : this.cash;
+        double other = this.other == null ? 0.0 : this.other;
 
+        this.totalAssets = deposit + savings + bond + fund + stock + cash + other;
+
+    }
 }
