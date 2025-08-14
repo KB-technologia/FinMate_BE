7f58c86d69664ff8278af16a964cd5243f8c4d97
mojiri00 <moguricamera@gmail.com>
2025-08-14 11:28:40 +0900

    ✨ CharacterResponseDTO 에 캐릭터 ID 필드 추가

--- src/main/java/org/finmate/character/dto/CharacterResponseDTO.java
+++ src/main/java/org/finmate/character/dto/CharacterResponseDTO.java
@@ -17,12 +17,16 @@ public class CharacterResponseDTO {
     @ApiModelProperty(value = "동물 캐릭터 이름", example = "고양이")
     private String animalName;
 
+    @ApiModelProperty(value = "사용자 동물 캐릭터 ID", example = "7")
+    private Long animalId;
+
     @ApiModelProperty(value = "동물 캐릭터 이미지 경로")
     private String animalImage;
 
-    public static CharacterResponseDTO from(final String userCharacterName, final String userCharacterImage) {
+    public static CharacterResponseDTO from(final String userCharacterName, final Long userAnimalId, final String userCharacterImage) {
         return CharacterResponseDTO.builder()
                 .animalName(userCharacterName)
+                .animalId(userAnimalId)
                 .animalImage(userCharacterImage)
                 .build();
     }
